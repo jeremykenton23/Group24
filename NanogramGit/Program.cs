3f37271c5844b9f5e2e5b499d28d34e3c208c665
JeremyKenton23 <jeremy.kenton@icloud.com>
2025-03-24 18:18:07 +0100

    Initial commit - Nonogram project in .NET 8

--- NanogramGit/Program.cs
+++ NanogramGit/Program.cs
@@ -1,52 +1,266 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Drawing;
-using System.Windows.Forms;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Threading;
+using System.Windows.Forms;
 
 namespace NonogramApp
 {
+    public abstract class PuzzleGame
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool[,] Grid { get; protected set; } = null!;
+        public List<List<int>> RowHints { get; protected set; } = null!;
+        public List<List<int>> ColumnHints { get; protected set; } = null!;
+
+        public abstract void GeneratePuzzle();
+        public abstract bool SolvePuzzle();
+        public abstract void ShowSolution();
+        public abstract bool CheckUserSolution();
+    }
+
+    public class NonogramGame : PuzzleGame
+    {
+        private bool[,] solutionGrid = null!;
+
+        public NonogramGame(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Grid = new bool[width, height];
+            solutionGrid = new bool[width, height];
+            RowHints = new List<List<int>>();
+            ColumnHints = new List<List<int>>();
+        }
+
+        public override void GeneratePuzzle()
+        {
+            try
+            {
+                var rand = new Random();
+                int fillRate = rand.Next(30, 60);
+
+                for (int i = 0; i < Width; i++)
+                    for (int j = 0; j < Height; j++)
+                        solutionGrid[i, j] = rand.Next(100) < fillRate;
+
+                GenerateHintsFromSolution();
+                HideSolution();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating puzzle: " + ex.Message);
+            }
+        }
+
+        public override bool SolvePuzzle()
+        {
+            try
+            {
+                ShowSolution();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error solving puzzle: " + ex.Message);
+                return false;
+            }
+        }
+
+        public override void ShowSolution()
+        {
+            Array.Copy(solutionGrid, Grid, solutionGrid.Length);
+        }
+
+        public override bool CheckUserSolution()
+        {
+            for (int i = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                    if (Grid[i, j] != solutionGrid[i, j])
+                        return false;
+            return true;
+        }
+
+        private void GenerateHintsFromSolution()
+        {
+            RowHints.Clear();
+            ColumnHints.Clear();
+
+            for (int y = 0; y < Height; y++)
+                RowHints.Add(GetHintsForLine(solutionGrid, y, true));
+
+            for (int x = 0; x < Width; x++)
+                ColumnHints.Add(GetHintsForLine(solutionGrid, x, false));
+        }
+
+        private List<int> GetHintsForLine(bool[,] grid, int index, bool isRow)
+        {
+            var hints = new List<int>();
+            int count = 0;
+            int length = isRow ? Width : Height;
+
+            for (int i = 0; i < length; i++)
+            {
+                bool value = isRow ? grid[i, index] : grid[index, i];
+                if (value) count++;
+                else if (count > 0)
+                {
+                    hints.Add(count);
+                    count = 0;
+                }
+            }
+
+            if (count > 0) hints.Add(count);
+            return hints.Count == 0 ? new List<int> { 0 } : hints;
+        }
+
+        private void HideSolution()
+        {
+            Grid = new bool[Width, Height];
+        }
+    }
+
+    public class GameController
+    {
+        private readonly int width;
+        private readonly int height;
+        private readonly int puzzleCount;
+        public List<PuzzleGame> Puzzles { get; private set; }
+
+        public GameController(int width, int height, int puzzleCount)
+        {
+            this.width = width;
+            this.height = height;
+            this.puzzleCount = puzzleCount;
+            Puzzles = new List<PuzzleGame>();
+        }
+
+        public void GenerateAllPuzzles()
+        {
+            try
+            {
+                Puzzles.Clear();
+                for (int i = 0; i < puzzleCount; i++)
+                {
+                    var game = new NonogramGame(width, height);
+                    game.GeneratePuzzle();
+                    Puzzles.Add(game);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating puzzles: " + ex.Message);
+            }
+        }
+    }
+
     public class MainForm : Form
     {
-        private NonogramGame game;
-        private int currentPuzzleIndex = 0;
-        private List<NonogramGame> puzzles;
+        private PuzzleGame game = null!;
+        private int currentPuzzleIndex = -1;
+        private GameController controller = null!;
         private const int cellSize = 30;
-        private Button nextPuzzleButton;
-        private Button solvePuzzleButton;
+        private Button nextPuzzleButton = null!;
+        private Button solvePuzzleButton = null!;
+        private Button solveAsyncButton = null!;
+        private Button checkSolutionButton = null!;
+        private System.Windows.Forms.Timer refreshTimer = null!;
 
         public MainForm()
         {
             this.Text = "Nonogram App";
-            this.ClientSize = new Size(400, 450);
+            this.ClientSize = new Size(700, 600);
             this.DoubleBuffered = true;
 
-            nextPuzzleButton = new Button { Text = "Volgende Puzzel", Location = new Point(10, 400), Size = new Size(120, 30) };
+            controller = new GameController(10, 10, 5);
+            controller.GenerateAllPuzzles();
+
+            InitializeButtons();
+            SetupTimer();
+            LoadNextPuzzle();
+        }
+
+        private void InitializeButtons()
+        {
+            nextPuzzleButton = new Button { Text = "Next Puzzle", Location = new Point(10, 520), Size = new Size(120, 30) };
             nextPuzzleButton.Click += (sender, e) => LoadNextPuzzle();
             this.Controls.Add(nextPuzzleButton);
 
-            solvePuzzleButton = new Button { Text = "Los Puzzel Op", Location = new Point(150, 400), Size = new Size(120, 30) };
-            solvePuzzleButton.Click += async (sender, e) => await SolvePuzzleAsync();
+            solvePuzzleButton = new Button { Text = "Show Solution", Location = new Point(150, 520), Size = new Size(120, 30) };
+            solvePuzzleButton.Click += (sender, e) => ShowSolution();
             this.Controls.Add(solvePuzzleButton);
 
-            puzzles = NonogramGame.GenerateUniquePuzzles(10, 10, 12);
-            LoadNextPuzzle();
+            solveAsyncButton = new Button { Text = "Solve (Async)", Location = new Point(290, 520), Size = new Size(120, 30) };
+            solveAsyncButton.Click += async (sender, e) => await SolvePuzzleAsync();
+            this.Controls.Add(solveAsyncButton);
+
+            checkSolutionButton = new Button { Text = "Check Solution", Location = new Point(430, 520), Size = new Size(120, 30) };
+            checkSolutionButton.Click += (sender, e) => CheckSolution();
+            this.Controls.Add(checkSolutionButton);
+        }
+
+        private void SetupTimer()
+        {
+            refreshTimer = new System.Windows.Forms.Timer();
+            refreshTimer.Interval = 100;
+            refreshTimer.Tick += (s, e) => Invalidate();
+            refreshTimer.Start();
         }
 
         private void LoadNextPuzzle()
         {
-            currentPuzzleIndex = (currentPuzzleIndex + 1) % puzzles.Count;
-            game = puzzles[currentPuzzleIndex];
-            this.Invalidate();
+            currentPuzzleIndex = (currentPuzzleIndex + 1) % controller.Puzzles.Count;
+            game = controller.Puzzles[currentPuzzleIndex];
+            Invalidate();
         }
 
         private async Task SolvePuzzleAsync()
         {
-            await Task.Run(() => game.Solve());
-            this.Invalidate();
+            solveAsyncButton.Enabled = false;
+            try
+            {
+                bool solved = await Task.Run(() => game.SolvePuzzle());
+                MessageBox.Show(solved ? "Puzzle solved!" : "Could not solve puzzle.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during async solve: " + ex.Message);
+            }
+            finally
+            {
+                solveAsyncButton.Enabled = true;
+                Invalidate();
+            }
+        }
+
+        private void ShowSolution()
+        {
+            try
+            {
+                game.ShowSolution();
+                Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error showing solution: " + ex.Message);
+            }
+        }
+
+        private void CheckSolution()
+        {
+            try
+            {
+                if (game.CheckUserSolution())
+                    MessageBox.Show("Well done! Correct solution.");
+                else
+                    MessageBox.Show("Oops! Incorrect solution.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error checking solution: " + ex.Message);
+            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -54,84 +268,58 @@ namespace NonogramApp
             base.OnPaint(e);
             if (game == null) return;
 
+            int offset = 100;
+
             for (int i = 0; i < game.Width; i++)
             {
                 for (int j = 0; j < game.Height; j++)
                 {
-                    Rectangle rect = new Rectangle(i * cellSize, j * cellSize, cellSize, cellSize);
+                    Rectangle rect = new Rectangle(offset + i * cellSize, offset + j * cellSize, cellSize, cellSize);
                     e.Graphics.FillRectangle(game.Grid[i, j] ? Brushes.Black : Brushes.White, rect);
                     e.Graphics.DrawRectangle(Pens.Black, rect);
                 }
+            }
+
+            for (int i = 0; i < game.Width; i++)
+            {
+                string hint = string.Join(" ", game.ColumnHints[i]);
+                e.Graphics.DrawString(hint, DefaultFont, Brushes.Black, offset + i * cellSize, offset - 20);
+            }
+
+            for (int j = 0; j < game.Height; j++)
+            {
+                string hint = string.Join(" ", game.RowHints[j]);
+                e.Graphics.DrawString(hint, DefaultFont, Brushes.Black, offset - 40, offset + j * cellSize);
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            try
+            {
+                int offset = 100;
+                int x = (e.X - offset) / cellSize;
+                int y = (e.Y - offset) / cellSize;
+
+                if (x >= 0 && x < game.Width && y >= 0 && y < game.Height)
+                {
+                    game.Grid[x, y] = !game.Grid[x, y];
+                    Invalidate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mouse click error: " + ex.Message);
             }
         }
 
         [STAThread]
         static void Main()
         {
-            System.Windows.Forms.Application.EnableVisualStyles();
-            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new MainForm());
-        }
-    }
-
-    public class NonogramGame
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public bool[,] Grid { get; set; }
-
-        public NonogramGame(int width, int height)
-        {
-            Width = width;
-            Height = height;
-            Grid = new bool[width, height];
-        }
-
-        public static List<NonogramGame> GenerateUniquePuzzles(int width, int height, int count)
-        {
-            var games = new List<NonogramGame>();
-            HashSet<string> uniqueHashes = new HashSet<string>();
-            Random rand = new Random();
-
-            while (games.Count < count)
-            {
-                var game = new NonogramGame(width, height);
-                for (int i = 0; i < width; i++)
-                    for (int j = 0; j < height; j++)
-                        game.Grid[i, j] = rand.Next(2) == 1;
-
-                string hash = string.Join("", FlattenGrid(game.Grid));
-                if (uniqueHashes.Add(hash))
-                {
-                    games.Add(game);
-                }
-            }
-            return games;
-        }
-
-        private static IEnumerable<string> FlattenGrid(bool[,] grid)
-        {
-            for (int i = 0; i < grid.GetLength(0); i++)
-                for (int j = 0; j < grid.GetLength(1); j++)
-                    yield return grid[i, j] ? "1" : "0";
-        }
-
-        public void Solve()
-        {
-            for (int i = 0; i < Width; i++)
-                for (int j = 0; j < Height; j++)
-                    Grid[i, j] = (i + j) % 2 == 0; // Simpel patroon voor oplossing
-        }
-
-        public void CalculateHints()
-        {
-            // Voorbeeld hintberekening (extra algoritme)
-            // Dit kan later worden verbeterd om correcte Nonogram hints te genereren
-        }
-
-        public void ImproveGeneration()
-        {
-            // Extra geavanceerd generatie-algoritme (derde algoritme)
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
         }
     }
 }
--- NanogramGit/obj/Debug/net8.0-windows/NanogramGit.AssemblyInfo.cs
+++ NanogramGit/obj/Debug/net8.0-windows/NanogramGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanogramGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db11f9a62092b220f540c0e6e1ea8ec219a6aaac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf8094cc79948e65a28dc28fa890ea0584c9c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanogramGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanogramGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
